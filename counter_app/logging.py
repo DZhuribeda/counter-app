c0bf71bc917df301504fb77883a4393a14c00308
DZhuribeda <zhurbikk@gmail.com>
2022-04-08 22:02:39 +0300

    fix logging issues

--- counter_app/logging.py
+++ counter_app/logging.py
@@ -1,5 +1,3 @@
-import logging
-import sys
 from opentelemetry import trace
 import structlog
 
@@ -14,10 +12,6 @@ def trace_processor(logger, log_method, event_dict):
 def logging_setup(settings):
     log_level = settings.log_level()
     processors = [
-        # If log level is too low, abort pipeline and throw away log entry.
-        structlog.stdlib.filter_by_level,
-        # Add the name of the logger to event dict.
-        structlog.stdlib.add_logger_name,
         # Add log level to event dict.
         structlog.stdlib.add_log_level,
         # Perform %-style formatting.
@@ -50,21 +44,6 @@ def logging_setup(settings):
 
     structlog.configure(
         processors=processors,
-        # `wrapper_class` is the bound logger that you get back from
-        # get_logger(). This one imitates the API of `logging.Logger`.
-        wrapper_class=structlog.stdlib.BoundLogger,
-        # `logger_factory` is used to create wrapped loggers that are used for
-        # OUTPUT. This one returns a `logging.Logger`. The final value (a JSON
-        # string) from the final processor (`JSONRenderer`) will be passed to
-        # the method of the same name as that you've called on the bound logger.
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        # Effectively freeze configuration after creating the first bound
-        # logger.
+        wrapper_class=structlog.make_filtering_bound_logger(int(log_level)),
         cache_logger_on_first_use=True,
-    )
-
-    logging.basicConfig(
-        format="%(message)s",
-        stream=sys.stdout,
-        level=log_level,
     )
